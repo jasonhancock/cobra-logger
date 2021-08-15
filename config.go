6953edabee165d57ef7e5a495d3913a90d98fd22
Jason Hancock <jason@jasonhancock.com>
2021-08-14 23:09:27 -0700

    initial commit

---
+++ config.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"io"
+	"strings"
+
+	"github.com/graymeta/env"
+	"github.com/jasonhancock/go-logger"
+	"github.com/spf13/cobra"
+)
+
+type Config struct {
+	Level string
+	name  string
+}
+
+func NewConfig(cmd *cobra.Command) *Config {
+	pieces := strings.Fields(cmd.Use)
+	c := &Config{
+		name: pieces[0],
+	}
+
+	cmd.Flags().StringVar(
+		&c.Level,
+		"log-level",
+		env.GetenvWithDefault("LOG_LEVEL", "info"),
+		"Log level (all|err|warn|info|debug",
+	)
+
+	return c
+}
+
+// Logger gets the logger
+func (cfg *Config) Logger(w io.Writer, keyvals ...interface{}) *logger.L {
+	return logger.New(w, cfg.name, cfg.Level, keyvals...)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jasonhancock/cobra-logger
+
+go 1.16
+
+require (
+	github.com/graymeta/env v0.0.2 // indirect
+	github.com/jasonhancock/go-logger v0.0.0-20210815060453-424b78e4dccf // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+)
